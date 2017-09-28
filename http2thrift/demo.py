c5990815cf513612639e4d2d65925b718e2764e3
root <account-login@users.noreply.github.com>
2017-09-28 10:55:32 +0800

    init

---
+++ http2thrift/demo.py
@@ -0,0 +1,74 @@
+from __future__ import (unicode_literals, print_function, division, absolute_import)
+
+import traceback
+
+from thriftpy.thrift import TApplicationException, TException
+from thriftpy.protocol.json import struct_to_json, struct_to_obj
+
+
+def get_args_obj(service, method, args_dict):
+    args_obj = getattr(service, method + '_args')()
+    struct_to_obj(args_dict, args_obj)
+    return args_obj
+
+
+def get_result_obj(service, method):
+    return getattr(service, method + '_result')()
+
+
+def handle_exception(exc, result):
+    for k in sorted(result.thrift_spec):
+        if result.thrift_spec[k][1] == "success":
+            continue
+
+        _, exc_name, exc_cls, _ = result.thrift_spec[k]
+        if isinstance(exc, exc_cls):
+            setattr(result, exc_name, exc)
+            return True
+    else:
+        return False
+
+
+def call_method(service, handler, method, args, result):
+    try:
+        f = getattr(handler, method)
+    except AttributeError:
+        raise TApplicationException(
+            TApplicationException.INTERNAL_ERROR, 'method not implemented: %s' % (method,))
+
+    args_list = [getattr(args, args.thrift_spec[k][1]) for k in sorted(args.thrift_spec)]
+
+    try:
+        result.success = f(*args_list)
+    except Exception as exc:
+        if not handle_exception(exc, result):
+            raise
+
+
+def call_method_with_dict(service, handler, method, args_dict):
+    if method not in service.thrift_services:
+        raise TApplicationException(TApplicationException.UNKNOWN_METHOD)
+
+    args = get_args_obj(service, method, args_dict)
+    result = get_result_obj(service, method)
+
+    call_method(service, handler, method, args, result)
+    return result
+
+
+def call_method_wrapped(service, handler, method, args_dict):
+    exception = None
+    try:
+        res = call_method_with_dict(service, handler, method, args_dict)
+    except TException as texc:
+        exception = texc
+    except Exception as exc:
+        traceback.print_exc()
+        exception = TApplicationException(
+            TApplicationException.INTERNAL_ERROR, 'uncaught exception: %r' % exc)
+    else:
+        return struct_to_json(res)
+
+    # exception
+    assert exception is not None
+    return dict(exception=struct_to_json(exception))
